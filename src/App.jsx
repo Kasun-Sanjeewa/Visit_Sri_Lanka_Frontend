cb50af674787b5e25aa9d9a5e23220cbf5198969
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-20 23:51:42 +0530

    Nav Bar created

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import { useState } from 'react'
 import Cards from './card'
 import { Hotels } from "./Hotels";
 import './App.css'
+import Registration from './Components/registration';
+
 
 function App() {
 
   return (
     <>
-    {Hotels.map((card) => {
-      return (
-        <>
-        <div className='card_list'>
-          <Cards {...card} key={card.id} />
-        </div>
-        </>
-      )})}
+      <nav className='nav-bar'>
+        <ul>
+          <li> <button className='register-btn'>Register</button></li>
+        </ul>
+      </nav>
+      {Hotels.map((card) => {
+        return (
+          <>
+            <div className='card_list'>
+              <Cards {...card} key={card.id} />
+            </div>
+          </>
+        )
+      })}
+
+      <Registration />
     </>
   )
 }
