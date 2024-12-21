abac5902a06f8a6b69d853a5f3f8a6a30c5db769
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-21 12:00:20 +0530

    ok

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar">
+            <div className="navbar-left">
+                <a href="/" className="logo">
+                    Visit Sri Lankal
+                </a>
+            </div>
+            <div className="navbar-center">
+                <ul className="nav-links">
+                    <li>
+                        <a href="/products">Places</a>
+                    </li>
+                    <li>
+                        <a href="/about">About Us</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                <a href="/account" className="user-icon">
+                    <i className="fas fa-user"></i>
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
