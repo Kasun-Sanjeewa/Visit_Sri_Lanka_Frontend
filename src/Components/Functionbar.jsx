3109992e300444603daaeba9216e41671034de9f
Kasun3865 <ict21945@fot.sjp.ac.lk>
2024-12-25 01:09:26 +0530

    funtion-bar
    funtion-bar

--- src/Components/Functionbar.jsx
+++ src/Components/Functionbar.jsx
@@ -1,39 +1,4 @@
 const Functionbar = () => {
-<<<<<<< Updated upstream
-    return (
-      <div className="function_bar">
-        <ul className="list_link">
-          <li>
-            <a href="/beaches">Beaches</a>
-          </li>
-          <li>
-            <a href="/Lake_Front">Lakes</a>
-          </li>
-          <li>
-            <a href="/Hiking">Hiking</a>
-          </li>
-          <li>
-            <a href="/Camping">Camping</a>
-          </li>
-          <li>
-            <a href="/Forests">Forests</a>
-          </li>
-          <li>
-            <a href="/Religius">Religius Places</a>
-          </li>
-          <li>
-            <a href="/Rooms">Rooms</a>
-          </li>
-          <li>
-            <a href="/Cabana">Cabana</a>
-          </li>
-          <li>
-            <a href="/Hotels">Hotels</a>
-          </li>
-        </ul>
-      </div>
-    );
-=======
   return (
     <div className="function_bar">
       <ul className="list_link">
@@ -76,7 +41,6 @@ const Functionbar = () => {
       </ul>
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default Functionbar;
