f7bc0fe5843349b9e7d83f99894bfb75fbf44baa
Asela Sandaruwan <108016651+Asela2001@users.noreply.github.com>
2024-12-28 16:12:39 +0530

    Re publish

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -5,24 +5,29 @@ const Cards = (props) => {
   const [index, setIndex] = useState(0);
 
   const handleNext = () => {
-    setIndex((prevIndex) => (prevIndex + 1) % image.length); // Loop forward
+    setIndex((prevIndex) => Math.min(prevIndex + 1, image.length - 1));
   };
 
   const handlePrev = () => {
-    setIndex((prevIndex) =>
-      prevIndex === 0 ? image.length - 1 : prevIndex - 1
-    ); // Loop backward
-    //Change the tags
+    setIndex((prevIndex) => Math.max(prevIndex - 1, 0));
   };
 
   return (
     <section className="card">
       <div className="img_container">
-        <button type="button" onClick={handlePrev} className="l_btn">
+        <button
+          type="button"
+          onClick={handlePrev}
+          className={`l_btn ${index === 0 ? "hidden" : ""}`}
+        >
           {"<"}
         </button>
         <img src={image[index]} alt={title} />
-        <button type="button" onClick={handleNext} className="R_btn">
+        <button
+          type="button"
+          onClick={handleNext}
+          className={`R_btn ${index === image.length - 1 ? "hidden" : ""}`}
+        >
           {">"}
         </button>
       </div>
@@ -32,11 +37,6 @@ const Cards = (props) => {
           <button type="button">Book Vehicle</button>
           <button type="button">Map</button>
         </div>
-
-        <h2>{title}</h2>
-        <button type="button" className="book-vehicle-btn">Book Vehicle</button>
-        <button type="button">Map</button>
-
       </div>
     </section>
   );
